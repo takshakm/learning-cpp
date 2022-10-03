e30cce00451f5f29669f9968da2761726a02a70d
Takshak Mudgal <takshakmudgal@gmail.com>
2022-10-03 18:21:46 +0530

    files created and deleted

---
+++ displaynum.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+int main(){
+    int fav_num;
+    std::cout <<"Enter your favoruit number between 1 and 100: ";
+    std::cin >> fav_num;
+    std::cout <<"Amazing that's my favoruit number too!" <<std::endl;
+    std::cout <<"No really, "<< fav_num<< " is my favoruit number!";
+    return 0;    
+}
