9df24a88abd8b3bee7203e1924cc3a38a320ee56
Takshak Mudgal <takshakmudgal@gmail.com>
2022-10-03 22:06:54 +0530

    using namespace std;

--- displaynum.cpp
+++ displaynum.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
+using namespace std;
 int main(){
     int fav_num;
-    std::cout <<"Enter your favoruit number between 1 and 100: ";
-    std::cin >> fav_num;
-    std::cout <<"Amazing that's my favoruit number too!" <<std::endl;
-    std::cout <<"No really, "<< fav_num<< " is my favoruit number!";
+    cout <<"Enter your favoruit number between 1 and 100: ";
+    cin >> fav_num;
+    cout <<"Amazing that's my favoruit number too!" <<std::endl;
+    cout <<"No really, "<< fav_num<< " is my favoruit number!";
     return 0;    
 }
