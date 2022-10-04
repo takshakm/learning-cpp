723ea8157d0c905b3aadac4f3e8fd8715959481d
Takshak Mudgal <takshakmudgal@gmail.com>
2022-10-04 08:52:21 +0530

    ''

--- calc_area.cpp
+++ calc_area.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int len {11}, breath {12};
-    cout<<"area is " <<len*breath;
+    cout<<"enter the width of room ";
+    int room_width{0};
+    cin>>room_width;
+    cout<<"enter length of the room ";
+    int room_length{0};
+    cin>>room_length;
+    cout<<"the area of the room is "<<room_width*room_length<<" sq feet"<<endl;
     return 0;
 }
-    
