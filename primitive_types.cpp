051f0f85774b94670092c9db95599a4f52921a42
Takshak Mudgal <takshakmudgal@gmail.com>
2022-10-04 09:25:40 +0530

    ''

--- primitive_types.cpp
+++ primitive_types.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 using namespace std;
 int main(){
-    unsigned long long int number_of_people{362'535'643'574'000};
-    cout<<number_of_people;
+    short value1 {3000};
+    short value2 {1000};
+    long product{value1*value2};
+    cout<<product<<endl;
     return 0;
 }
