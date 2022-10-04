0c978e854d3932658c1cbed7b5a9837f3f28a5e8
Takshak Mudgal <takshakmudgal@gmail.com>
2022-10-04 11:46:39 +0530

    made some changes to frank's cleaning program

--- frank_carpet_cleaning_service.cpp
+++ frank_carpet_cleaning_service.cpp
@@ -1,18 +1,25 @@
 #include<iostream>
 using namespace std;
 int main(){
-    const int per_room= 30;
-    cout<<"***FRANK'S CARPET CLEANING SERVICE***"<<endl;
-    int num_of_rooms;
-    cout<<"Enter the number of rooms "<<endl;
-    cin>>num_of_rooms;
-    cout<<"price per room $"<<per_room<<endl;
-    const int cost=per_room*num_of_rooms;
-    cout<<"cost $"<<cost<<endl;
-    const double tax=cost*0.06;
-    cout<<"tax $"<<tax<<endl;
-    const float ttl=cost+tax;
-    cout<<"total estimate $"<<ttl<<endl;
-    cout<<"this estimate is valid for 30 days";
+    cout<<"Hello, welcome to Frank's Cleaning Service\n"<<endl;
+    int small_rooms;
+    int large_rooms;
+    const int per_small_room=25;
+    const int per_large_room=35;
+    cout<<"Number of small rooms: ";
+    cin>>small_rooms;
+    cout<<"Number of large rooms: ";
+    cin>>large_rooms;
+    cout<<"Price per small room: $"<<per_small_room <<endl;
+    cout<<"Price per large room: $"<<per_large_room <<endl;
+    int small{small_rooms*per_small_room};
+    int large{large_rooms*per_large_room};
+    int a{small+large};
+    cout<<"Cost: $"<<small+large<<endl;
+    double tax{a*0.06};
+    cout<<"Tax: $"<<tax<<endl;
+    cout<<"=================================================="<<endl;
+    cout<<"Total Estimate: $"<<a+tax<<endl;
+    cout<<"This estimate is valid for 30 days"<<endl;
     return 0;   
 }
