f0009ec006f5ca85dbd199fbed8c41ea4433cae6
Takshak Mudgal <takshakmudgal@gmail.com>
2022-10-04 10:56:06 +0530

    made a carpet cleaning price estimate program

---
+++ size_of_operator.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<climits>
+#include<cfloat>
+using namespace std;
+int main(){
+    cout<<"size of information "<<endl<<endl;
+    cout<<"char "<<sizeof(char)<<endl;
+    cout<<"int "<<sizeof(int)<<endl;
+    cout<<"unsigned int "<<sizeof(unsigned int)<<endl;
+    cout<<"short "<<sizeof(short)<<endl;
+    cout<<"long "<<sizeof(long)<<endl;
+    cout<<"long long "<<sizeof(long long)<<endl;
+    cout<<"float "<<sizeof(float)<<endl;
+    cout<<"double "<<sizeof(double)<<endl;
+    cout<<"long double "<<sizeof(long double)<<endl;
+    return 0;
+}
